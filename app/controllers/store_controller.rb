381a6cbc7348c2a13118bf0481d5f6149da4f822
Andrei Klepets <a.klepets@unlitsolutions.com>
2017-05-07 20:56:49 +0300

    cart creation playtime

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter]=1
+    else
+      session[:counter]+=1
+    end
+    if session[:counter] > 5
+      flash[:notice] = "Bro, you've been here #{session[:counter]} #{'time'.pluralize(session[:counter])}"
+    end
   end
 end
