3224386194f5b10232f14990e1abbf6ab94d8fdf
Andrei Klepets <a.klepets@unlitsolutions.com>
2017-07-02 22:40:07 +0300

    task e add cart unit tests for add product

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "add two similar products to cart" do
+    @cart = Cart.create
+    @cart.add_product(products(:ruby).id).save!
+    @cart.add_product(products(:ruby).id).save!
+    assert_equal 1, @cart.line_items.size
+    assert_equal 99.0, @cart.total_price
+  end
+
+  test "add two unique products to cart" do
+    @cart = Cart.create
+    @cart.add_product(products(:ruby).id).save!
+    @cart.add_product(products(:two).id).save!
+    assert_equal 2, @cart.line_items.size
+    assert_equal 59.49, @cart.total_price
+  end
 end
