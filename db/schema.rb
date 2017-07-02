9f167556bf46a534fbf794fca3342e10c5167ccb
Andrei Klepets <a.klepets@unlitsolutions.com>
2017-07-02 13:46:14 +0300

    task e line_items migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509140628) do
+ActiveRecord::Schema.define(version: 20170625115126) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170509140628) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity",   default: 1
+    t.decimal  "price",      default: 0.0
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
@@ -33,7 +34,7 @@ ActiveRecord::Schema.define(version: 20170509140628) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
+    t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
