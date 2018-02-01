675f4222fe34fec5bb531c7b7c7f5c8f2414b5c6
Andrei Klepets <a.klepets@unlitsolutions.com>
2018-02-01 18:52:49 +0300

    fix for one test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029192232) do
+ActiveRecord::Schema.define(version: 20171221160152) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -41,11 +41,18 @@ ActiveRecord::Schema.define(version: 20171029192232) do
     t.datetime "updated_at"
   end
 
+  create_table "payment_types", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
+    t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
