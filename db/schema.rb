9a60686d140463bd8c9da0ff83c0c789bfe77fee
Andrei Klepets <a.klepets@unlitsolutions.com>
2018-02-06 17:32:41 +0300

    i1: adding users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171029192232) do
+ActiveRecord::Schema.define(version: 20180206134606) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -41,11 +41,25 @@ ActiveRecord::Schema.define(version: 20171029192232) do
     t.datetime "updated_at"
   end
 
+  create_table "payment_types", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "products", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
     t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
